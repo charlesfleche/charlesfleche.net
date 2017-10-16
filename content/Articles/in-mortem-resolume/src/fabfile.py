6abbbdb119d0bb1f14c3e2b02158d7716a5d574e
Charles Flèche <charles.fleche@free.fr>
2017-10-17 01:56:19 +0700

    Make images progressive

--- content/Articles/in-mortem-resolume/src/fabfile.py
+++ content/Articles/in-mortem-resolume/src/fabfile.py
@@ -6,16 +6,17 @@ p = {
     'hwide': '640',
     'd': '..',
     'tmp': '/tmp/image.png',
-    'q': '-quality 60'
+    'q': '-quality 60',
+    'o': '-strip -interlace plane'
 }
 
 def build():
     local('inkscape -z -e {tmp} -w {wwide} -h {hwide} banner.svg'.format(**p))
-    local('convert {tmp} {q} {d}/banner.jpg'.format(**p))
-    local('convert in-mortem-stage.jpg {q} {g} {d}/stage.jpg'.format(**p))
-    local('convert in-mortem-resolume.png {q} {g} {d}/resolume.jpg'.format(**p))
-    local('convert in-mortem-outputs.png {q} {g} {d}/outputs.jpg'.format(**p))
-    local('convert in-mortem-osc-foh.png {q} {g} {d}/osc-foh.jpg'.format(**p))
-    local('convert in-mortem-resolume-osc.png {q} {g} {d}/resolume-osc.jpg'.format(**p))
-    local('convert in-mortem-oscwidgets.png {q} {g} {d}/oscwidgets.jpg'.format(**p))
-    local('convert in-mortem-osc-stage.jpg {q} {g} {d}/osc-stage.jpg'.format(**p))
+    local('convert {o} {tmp} {q} {d}/banner.jpg'.format(**p))
+    local('convert in-mortem-stage.jpg {o} {q} {g} {d}/stage.jpg'.format(**p))
+    local('convert in-mortem-resolume.png {o} {q} {g} {d}/resolume.jpg'.format(**p))
+    local('convert in-mortem-outputs.png {o} {q} {g} {d}/outputs.jpg'.format(**p))
+    local('convert in-mortem-osc-foh.png {o} {q} {g} {d}/osc-foh.jpg'.format(**p))
+    local('convert in-mortem-resolume-osc.png {o} {q} {g} {d}/resolume-osc.jpg'.format(**p))
+    local('convert in-mortem-oscwidgets.png {o} {q} {g} {d}/oscwidgets.jpg'.format(**p))
+    local('convert in-mortem-osc-stage.jpg {o} {q} {g} {d}/osc-stage.jpg'.format(**p))
