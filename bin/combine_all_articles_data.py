2d492d9707e0aab1a2d337c19dd122d55cabd5c3
Charles Flèche <charles.fleche@free.fr>
2025-04-22 22:07:26 -0400

    Generate all articles json

--- bin/combine_all_articles_data.py
+++ bin/combine_all_articles_data.py
@@ -2,10 +2,14 @@ import argparse
 import json
 import sys
 
+import yaml
+
 parser = argparse.ArgumentParser()
 parser.add_argument("paths", nargs="+", type=argparse.FileType("r"))
 args = parser.parse_args()
 
-data = sorted([json.load(fp) for fp in args.paths], key=lambda d: d.get("date", ""))
+data = [yaml.safe_load(fp) for fp in args.paths]
+data = [d for d in data if d is not None]
+data = sorted(data, key=lambda d: d.get("date", ""))
 
 json.dump(data, sys.stdout, indent=2, sort_keys=True)
