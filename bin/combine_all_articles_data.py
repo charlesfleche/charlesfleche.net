407a1f8a2ea1a478fba7fc30c7223e0eb3ba151f
Charles Flèche <charles.fleche@ubisoft.com>
2025-05-01 13:34:25 -0400

    Nav

--- bin/combine_all_articles_data.py
+++ bin/combine_all_articles_data.py
@@ -1,7 +1,6 @@
 import argparse
 import copy
 import json
-import sys
 
 import yaml
 
@@ -13,9 +12,7 @@ args = parser.parse_args()
 
 global_data = yaml.safe_load(args.global_data)
 
-all_articles_data = {
-    "by_slug": {}
-}
+all_articles_data = {"by_slug": {}}
 for fp in args.paths:
     article_data = copy.deepcopy(global_data)
     article_data.update(json.load(fp))
@@ -33,11 +30,24 @@ for fp in args.paths:
         )
     )
 
+    article_data["rss"] = f"{article_data['netloc']}{article_data['rss_path']}"
+
+    article_data["nav"].insert(0, {"name": "Home", "href": article_data["netloc"]})
+    article_data["nav"].append({"name": "Rss", "href": article_data["rss"]})
+
     all_articles_data["by_slug"][article_data["slug"]] = article_data
 
-all_articles_data["by_date"] = list(reversed(sorted(
-    [article for article in all_articles_data["by_slug"].values() if article["category"] != "page"],
-    key=lambda article: article.get("date")
-)))
+all_articles_data["by_date"] = list(
+    reversed(
+        sorted(
+            [
+                article
+                for article in all_articles_data["by_slug"].values()
+                if article["category"] != "page"
+            ],
+            key=lambda article: article.get("date"),
+        )
+    )
+)
 
 json.dump(all_articles_data, args.out, indent=2, sort_keys=True)
