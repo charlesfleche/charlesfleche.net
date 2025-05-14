b31455466c12a11d060dd603bf2d1693dc588605
Charles Flèche <charles.fleche@free.fr>
2025-05-13 23:10:01 -0400

    Remove static dst before ln or cp

--- bin/ln_or_cp.py
+++ bin/ln_or_cp.py
@@ -8,6 +8,7 @@ parser.add_argument("dst", type=pathlib.Path)
 args = parser.parse_args()
 
 args.dst.parent.mkdir(parents=True, exist_ok=True)
+args.dst.unlink(missing_ok=True)
 try:
     args.dst.hardlink_to(args.src)
 except NotImplementedError:
